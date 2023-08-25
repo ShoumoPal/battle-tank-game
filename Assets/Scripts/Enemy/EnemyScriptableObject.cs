246d820216257b536cf4bd6136fe22a4094f32cc
ShoumoPal <arka.pal21@gmail.com>
2023-08-25 12:25:35 +0530

    Added Enemy ScriptableObjects

---
+++ Assets/Scripts/Enemy/EnemyScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyScriptableObject", menuName = "ScriptableObjects/NewEnemyScriptableObject")]
+public class EnemyScriptableObject : ScriptableObject
+{
+    public EnemyView enemyView;
+}
